a1a3a77344cd4ce75109c893862fa0b3bb8b4b23
Eduarda Barros <eduarda.santanasb@gmail.com>
2024-05-29 17:29:27 -0300

    modifica cor do botao de contato

--- app/src/main/java/com/example/lunchtray/ui/TelaInicial.kt
+++ app/src/main/java/com/example/lunchtray/ui/TelaInicial.kt
@@ -42,7 +42,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lunchtray.R
 import com.example.lunchtray.ui.theme.LunchTrayTheme
-
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -52,6 +52,7 @@ import androidx.compose.material.icons.filled.Bookmarks
 import androidx.compose.material3.Icon
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.lunchtray.ui.theme.md_theme_background_yellow
 import com.example.lunchtray.ui.theme.md_theme_light_tertiary
 
 @Composable
@@ -100,13 +101,15 @@ fun TelaInicial(
                     onClick = onStartOrderButtonClicked,
                     modifier = Modifier
                         .align(Alignment.BottomCenter)
-                        .padding(vertical = 30.dp)
+                        .padding(vertical = 30.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFEABC17)) // Defina a cor desejada aqui
                 ) {
                     Text(
                         stringResource(R.string.start_order),
                         style = MaterialTheme.typography.titleMedium,
                     )
                 }
+
             }
         }
 
