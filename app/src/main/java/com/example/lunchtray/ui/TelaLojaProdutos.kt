e6e0aa507cce7a3e0aabf7469754beabaaa73030
Mário Jorge <mario.00000846606@unicap.br>
2024-06-04 17:02:58 -0300

    links

--- app/src/main/java/com/example/lunchtray/ui/TelaLojaProdutos.kt
+++ app/src/main/java/com/example/lunchtray/ui/TelaLojaProdutos.kt
@@ -16,11 +16,12 @@
 
 package com.example.lunchtray.ui
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-//import androidx.compose.foundation.layout.FlowColumnScopeInstance.align
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -42,9 +43,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 import com.example.lunchtray.R
 import com.example.lunchtray.ui.theme.md_theme_background_yellow
 
@@ -54,18 +57,26 @@ import com.example.lunchtray.ui.theme.md_theme_background_yellow
 fun TelaLojaProdutos() {
 
     val (isVisible, setIsVisible) = remember { mutableStateOf(false) }
+
+
+    val context = LocalContext.current
+    val phoneNumber = "55XXXXXXXXXXX "
     Box(
         modifier = Modifier
             .background(Color.White)
             .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally)
+        {
+
             if (!isVisible) {
+
                 Button(
+
                     onClick = { setIsVisible(true) },
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFEABC17))
-                 ) {
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
+                ) {
                     Text(text = "Entrar na loja PE")
                 }
             } else {
@@ -82,7 +93,7 @@ fun TelaLojaProdutos() {
                                     shape = RoundedCornerShape(16.dp)
                                 )
                                 .width(180.dp)
-                                .height(200.dp)
+                                .height(330.dp)
                                 .padding(16.dp)
                         ) {
 
@@ -99,10 +110,40 @@ fun TelaLojaProdutos() {
 
                             Text(
                                 modifier = Modifier
-                                    .align(Alignment.BottomCenter),
+                                    .padding(top = 30.dp)
+                                    .align(Alignment.Center),
                                 text = "Coletor de poeira"
 
                             )
+
+                            Text(
+                                modifier = Modifier
+                                    .padding(top = 90.dp)
+                                    .align(Alignment.Center),
+                                text = "Apenas  20$ Reais",
+
+
+                                )
+
+                            Button(
+
+                                onClick = {
+
+                                    val uri = Uri.parse("https://wa.me/$phoneNumber")
+                                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                                    startActivity(context, intent, null)
+                                },
+                                modifier = Modifier
+                                    .width(120.dp)
+                                    .padding(5.dp)
+                                    .height(35.dp)
+
+                                    .align(Alignment.BottomCenter), shape = RoundedCornerShape(6.dp),
+                                colors = ButtonDefaults.buttonColors(Color.Green)
+
+                            ) {
+                                Text("Contato")
+                            }
 
                         }
                         Spacer(modifier = Modifier.width(16.dp))
@@ -115,7 +156,7 @@ fun TelaLojaProdutos() {
                                 )
                                 .width(180.dp)
                                 .padding(16.dp)
-                                .height(170.dp)
+                                .height(300.dp)
 
 
                         ) {
@@ -131,10 +172,40 @@ fun TelaLojaProdutos() {
 
                             Text(
                                 modifier = Modifier
-                                    .align(Alignment.BottomCenter),
+                                    .padding(top = 30.dp)
+                                    .align(Alignment.Center),
                                 text = "Segundo couro"
 
                             )
+
+                            Text(
+                                modifier = Modifier
+                                    .padding(top = 90.dp)
+                                    .align(Alignment.Center),
+                                text = "Apenas  30$ Reais",
+
+
+                                )
+
+                            Button(
+
+                                onClick = {
+
+                                    val uri = Uri.parse("https://wa.me/$phoneNumber")
+                                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                                    startActivity(context, intent, null)
+                                },
+                                modifier = Modifier
+                                    .width(120.dp)
+                                    .padding(5.dp)
+                                    .height(35.dp)
+
+                                    .align(Alignment.BottomCenter), shape = RoundedCornerShape(6.dp),
+                                colors = ButtonDefaults.buttonColors(Color.Green)
+
+                            ) {
+                                Text("Contato")
+                            }
                         }
                     }
 
@@ -150,7 +221,7 @@ fun TelaLojaProdutos() {
                                     shape = RoundedCornerShape(16.dp)
                                 )
                                 .width(180.dp)
-                                .height(200.dp)
+                                .height(330.dp)
                                 .padding(16.dp)
                         ) {
 
@@ -166,10 +237,40 @@ fun TelaLojaProdutos() {
 
                             Text(
                                 modifier = Modifier
-                                    .align(Alignment.BottomCenter),
+                                    .padding(top = 8.dp)
+                                    .align(Alignment.Center),
                                 text = "Amuleto"
 
                             )
+
+                            Text(
+                                modifier = Modifier
+                                    .padding(top = 90.dp)
+                                    .align(Alignment.Center),
+                                text = "Apenas  40$ Reais",
+
+
+                                )
+
+                            Button(
+
+                                onClick = {
+
+                                    val uri = Uri.parse("https://wa.me/$phoneNumber")
+                                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                                    startActivity(context, intent, null)
+                                },
+                                modifier = Modifier
+                                    .width(120.dp)
+                                    .padding(5.dp)
+                                    .height(35.dp)
+
+                                    .align(Alignment.BottomCenter), shape = RoundedCornerShape(6.dp),
+                                colors = ButtonDefaults.buttonColors(Color.Green)
+
+                            ) {
+                                Text("Contato")
+                            }
 
                         }
                         Spacer(modifier = Modifier.width(16.dp))
@@ -183,10 +284,11 @@ fun TelaLojaProdutos() {
                                 )
                                 .width(180.dp)
                                 .padding(16.dp)
-                                .height(170.dp)
+                                .height(300.dp)
 
 
                         ) {
+
 
                             Image(
                                 painter = painterResource(id = R.drawable.chapeu_pe),
@@ -195,14 +297,46 @@ fun TelaLojaProdutos() {
                                     //.fillMaxWidth()
                                     .size(140.dp)
                                     .padding(start = 8.dp)
+
                             )
 
                             Text(
                                 modifier = Modifier
-                                    .align(Alignment.BottomCenter),
-                                text = "Esconde chifre"
+                                    .padding(start = 8.dp)
+                                    .align(Alignment.Center),
+                                text = "Esconde chifre",
 
-                            )
+
+                                )
+
+                            Text(
+                                modifier = Modifier
+                                    .padding(top = 90.dp)
+                                    .align(Alignment.Center),
+                                text = "Apenas  50$ Reais",
+
+
+                                )
+
+                            Button(
+
+                                onClick = {
+
+                                    val uri = Uri.parse("https://wa.me/$phoneNumber")
+                                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                                    startActivity(context, intent, null)
+                                },
+                                modifier = Modifier
+                                    .width(120.dp)
+                                    .padding(5.dp)
+                                    .height(35.dp)
+
+                                    .align(Alignment.BottomCenter), shape = RoundedCornerShape(6.dp),
+                                colors = ButtonDefaults.buttonColors(Color.Green)
+
+                            ) {
+                                Text("Contato")
+                            }
                         }
                     }
                 }
@@ -216,9 +350,16 @@ fun TelaLojaProdutos() {
 }
 
 
+
+
+
 @Preview
 @Composable
 private fun LojaScreenPreview() {
     TelaLojaProdutos()
 }
+
+
+
+
 
